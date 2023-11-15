f59cb905e51898aac26a46e568966d09e755ea9f
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-11-15 12:03:06 +0100

    a

--- mirrorweb/mirrorweb/settings.py
+++ mirrorweb/mirrorweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%h)a6v68c-n-t60+k8b!)rd&(tbtyuy916e+mrh(k@05#c*z4a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
