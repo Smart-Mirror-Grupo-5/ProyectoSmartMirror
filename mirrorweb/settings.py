a26865164392b1ffc084641bbae075e65f64b867
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-11-15 16:13:03 +0100

    prueba4

--- mirrorweb/settings.py
+++ mirrorweb/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+# Ruta al directorio donde se almacenarán los archivos estáticos
+STATIC_ROOT = BASE_DIR / 'mirrorwebapp/static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
