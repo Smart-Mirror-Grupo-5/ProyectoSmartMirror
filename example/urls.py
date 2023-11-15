607405585400cacf9efb9279ebaef53bcc0537e0
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-11-15 19:14:14 +0100

    arreglado

--- example/urls.py
+++ example/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home', views.home,name="home"),
+    path('index/', views.index, name='index'),
 ]
 
 
--- example/views.py
+++ example/views.py
@@ -4,5 +4,5 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
+def index(request):
     return render(request, 'index.html')
