1b1396b317667a1691d0a38611225c882727c52f
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-11-15 18:41:29 +0100

    con esto y un bizcocho...

--- mirrorweb/settings.py
+++ mirrorweb/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,9 +120,7 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-STATIC_URL = '/mirrorwebapp/static/'
 
-# Ruta al directorio donde se almacenarán los archivos estáticos
-STATIC_ROOT = BASE_DIR / 'mirrorwebapp/static'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
