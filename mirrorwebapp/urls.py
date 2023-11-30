30eeb7a94d4701df6ad335e4068142c0bead3d07
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-11-30 15:57:07 +0100

    pagina en construccion

--- mirrorwebapp/urls.py
+++ mirrorwebapp/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('index/', views.index, name='index'),
+    path('login/', views.login, name='login'),
 ]
 
 
--- mirrorwebapp/views.py
+++ mirrorwebapp/views.py
@@ -4,5 +4,8 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def login(request):
+    return render(request, 'login.html')
+
 def index(request):
     return render(request, 'index.html')
